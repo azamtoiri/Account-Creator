c25dba96d2b948faefa552717aba1697f96c3cf9
azamtoiri <azamtoiri@gmail.com>
2022-11-17 13:04:04 +0300

    Add fake user agent. structured project

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
+import links
+
 from time import sleep
 from options import driver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-driver.get("https://www.google.com/account/about/")
-html = driver.find_element(By.TAG_NAME, "html")
+from input_data import *
 
-# region: Buttons: account details
-b_create_account = '//*[@id="h-js-header__drawer"]/div[2]/ul/li[1]/a'
-b_input_firstName = '//*[@id="firstName"]'
-b_input_lastName = '//*[@id="lastName"]'
-b_username = '//*[@id="username"]'
-b_password = '//*[@id="passwd"]/div[1]/div/div[1]/input'
-b_confirm_password = '//*[@id="confirm-passwd"]/div[1]/div/div[1]/input'
-b_next_1 = '//*[@id="accountDetailsNext"]/div/button/span'
-# endregion
+driver.get(f"{links.google_account}")
+
+html = driver.find_element(By.TAG_NAME, "html")
 
 
 def main():
--- options.py
+++ options.py
@@ -2,16 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from fake_useragent import UserAgent
+
+# fake user agent
+ua = UserAgent()
+useragent = ua.random
+
 # Options
 option = webdriver.ChromeOptions()
 
 # User Agent
-option.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                    "Chrome/107.0.0.0 Safari/537.36")
+option.add_argument(
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36')
 
 # WebDriver mode OFF
 option.add_argument("--disable-blink-features=AutomationControlled")
 
 # Drivers
-
 driver = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()), options=option)
