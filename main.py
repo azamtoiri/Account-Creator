f42eedef4cc740896f04f9abb546c4ac8ebe3c52
spike <azamtoiri@gmail.com>
2023-12-19 12:28:18 +0300

    [2] fix!: adding values and clicking next
    Generating Name, LastName and other data and inserting to boxes automatically. But still need a solution to bypass confirming phone number

--- main.py
+++ main.py
@@ -1,38 +1,97 @@
-from data import links
-
+import random
 from time import sleep
-from options import driver
+
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
-from data.buttons_xpath import *
+from data import links
+from data.lists_NLO import combined_first_names_array, last_name_array
+from options import driver, generate_password, alphabet
 
-driver.get(f"{links.google_account2}")
-driver.get_cookie()
+driver.get(f"{links.google_account}")
 
-html = driver.find_element(By.TAG_NAME, "html")
+GMAIL_ADDR = None
+# html = driver.find_element(By.TAG_NAME, "html")
 
 
 def main():
+    def click_next_button() -> None:
+        """Click all next buttons on pages: clicking next button and wait 2 sec"""
+        driver.find_element(By.CLASS_NAME, "VfPpkd-dgl2Hf-ppHlrf-sM5MNb").click()
+        sleep(2)
     try:
         sleep(2)
-        # driver.find_element(By.TAG_NAME, "use").click()
+        # get the firstname textbox
+        first_name = driver.find_element(By.ID, "firstName")
+        if first_name == None:
+            raise ValueError("First name TextBox not found")
+        first_name.clear()
+        # generate first_name
+        combined_first_names = (random.choice(combined_first_names_array))
+        # enter first name
+        first_name.send_keys(combined_first_names)
 
-        driver.find_element(By.XPATH, b_create_account).click()
+        # get the surname box
+        surname = driver.find_element(By.ID, "lastName")
+        surname.clear()
+        # getting random last_name
+        last_name = (random.choice(last_name_array))
+        # enter surname
+        surname.send_keys(last_name)
 
-        driver.find_element(By.XPATH, b_create_account_for_myself).click()
+        # click next button
+        click_next_button()
 
-        driver.find_element(By.XPATH, b_input_firstName).send_keys("lsdkfj;lasdfj;lasd")
+        # get day enter box
+        day = driver.find_element(By.ID, "day")
+        day.clear()
+        # enter day
+        day.send_keys((random.randint(1, 26)))
 
-        driver.find_element(By.XPATH, b_input_lastName).send_keys("sldfjsldfj")
+        # get month enter box
+        month_dropdown = driver.find_element(By.ID, "month")
+        month_select = Select(month_dropdown)
+        month_select.select_by_index(random.randint(1, 12))
 
-        driver.find_element(By.XPATH, b_username).send_keys("sdlkfjlkasd")
+        # get year text box
+        year_box = driver.find_element(By.ID, "year")
+        year_box.clear()
+        # enter year
+        year_box.send_keys(random.randint(1968, 2003))
 
-        driver.find_element(By.XPATH, b_password).send_keys("SDLfjsdlkjf;lasdjf")
-        driver.find_element(By.XPATH, b_confirm_password).send_keys("SDLfjsdlkjf;lasdjf")
-        driver.find_element(By.XPATH, b_next_1).click()
-        sleep(2)
+        # set male or femail
+        male_or_female = driver.find_element(By.ID, "gender")
+        male_or_female_select = Select(male_or_female)
+        # setting male or female
+        male_or_female_select.select_by_index(random.randint(1, 2))
+
+        # click next button
+        click_next_button()
+
+        # choose own address and press next
+        gmail_select = driver.find_element(By.ID, 'selectioni1')
+        GMAIL_ADDR = gmail_select.text
+        gmail_select.click()
+        # click next button
+        click_next_button()
+
+        # get password textbox
+        password = driver.find_element(By.CSS_SELECTOR, 'input[aria-label="Пароль"]')
+        password.clear()
+        # generate password
+        mypw = generate_password(alphabet, 8)
+        password.send_keys(mypw)
+
+        # confirm password
+        confirm_password = driver.find_element(By.CSS_SELECTOR, 'input[aria-label="Подтвердить"]')
+        confirm_password.clear()
+        confirm_password.send_keys(mypw)
+
+        # click next button
+        click_next_button()
+
     except Exception as ex:
-        print(ex)
+        print(ex['args'])
     finally:
         driver.close()
         driver.quit()
--- options.py
+++ options.py
@@ -1,6 +1,9 @@
-from selenium import webdriver
-from data.links import user_agent
+import random
+
 from fake_useragent import UserAgent
+from selenium import webdriver
+
+from data.links import user_agent
 
 # fake user agent
 ua = UserAgent()
@@ -11,10 +14,31 @@ option = webdriver.ChromeOptions()
 
 # User Agent
 option.add_argument(
-    f'User-Agent={user_agent}')
+    f'User-Agent={user_agent}'
+)
 
 # WebDriver mode OFF
 option.add_argument("--disable-blink-features=AutomationControlled")
 
 # Drivers
 driver = webdriver.Chrome("web-driver/chromedriver.exe", options=option)
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+def generate_password(alphabet: str, pw_length: int) -> str:
+    mypw = ""
+    for i in range(pw_length):
+        next_index = random.randrange(len(alphabet))
+        mypw = mypw + alphabet[next_index]
+
+    # replace 1 or 2 characters with a number
+    for i in range(random.randrange(1, 3)):
+        replace_index = random.randrange(len(mypw) // 2)
+        mypw = mypw[0:replace_index] + str(random.randrange(10)) + mypw[replace_index + 1:]
+
+    # replace 1 or 2 letters with an uppercase letter
+    for i in range(random.randrange(1, 3)):
+        replace_index = random.randrange(len(mypw) // 2, len(mypw))
+        mypw = mypw[0:replace_index] + mypw[replace_index].upper() + mypw[replace_index + 1:]
+
+    return mypw
